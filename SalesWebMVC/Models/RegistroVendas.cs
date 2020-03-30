93b27419bb6bf9a3d443d8c64a73c0b448c3da1b
Guilherme Lima <guigui410@gmail.com>
2020-03-30 14:52:45 -0300

    Adicionando Entidades e secunda migração

---
+++ SalesWebMVC/Models/RegistroVendas.cs
@@ -0,0 +1,29 @@
+﻿using SalesWebMVC.Models.Enums;
+using System;
+
+
+namespace SalesWebMVC.Models
+{
+    public class RegistroVendas
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public double Amout { get; set; }
+        public VendaStatus Status { get; set; }
+        public Vendedor Vendedor { get; set; }
+
+        public RegistroVendas()
+        {
+
+        }
+
+        public RegistroVendas(int id, DateTime data, double amout, VendaStatus status, Vendedor vendedor)
+        {
+            Id = id;
+            Data = data;
+            Amout = amout;
+            Status = status;
+            Vendedor = vendedor;
+        }
+    }
+}
---
+++ SalesWebMVC/Models/Vendedor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Models
+{
+    public class Vendedor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public double BaseSalario { get; set; }
+        public Departamento Departamento { get; set; }
+        public ICollection<RegistroVendas> Vendas { get; set; } = new List<RegistroVendas>();
+
+        public Vendedor()
+        {
+
+        }
+
+        public Vendedor(int id, string nome, string email, DateTime dataNascimento, double baseSalario, Departamento departamento)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            DataNascimento = dataNascimento;
+            BaseSalario = baseSalario;
+            Departamento = departamento;
+        }
+
+        public void AddVenda(RegistroVendas venda)
+        {
+            Vendas.Add(venda);
+        }
+
+        public void RemoveVenda(RegistroVendas venda)
+        {
+            Vendas.Remove(venda);
+        }
+
+        public double TotalVendas(DateTime initial, DateTime Fim)
+        {
+            return Vendas.Where(x => x.Data >= initial && x.Data <= Fim).Sum(x => x.Amout);
+        }
+    }
+}
